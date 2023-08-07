f00102090cec73c9f2e7f76de7c543f74c27a028
Sreeni002 <sreenu.reddy002@gmail.com>
2023-08-07 11:37:43 +0530

    change in bease encode

--- main.tf
+++ main.tf
@@ -41,5 +41,5 @@ resource "aws_instance" "rabbitmq" {
     kms_key_id = var.kms_arn
   }
 
-  user_data = base64decode(templatefile("${path.module}/userdata.sh", {rabbitmq_appuser_password = data.aws_ssm_parameter.rabbitmq_appuser_password.value}))
+  user_data = base64encode(templatefile("${path.module}/userdata.sh", {rabbitmq_appuser_password = data.aws_ssm_parameter.rabbitmq_appuser_password.value}))
 }
